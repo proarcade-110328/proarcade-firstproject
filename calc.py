2a46bd78eacfb123a8ccd8e0141c64a2c09a6bdf
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-11-15 08:00:28 +0900

    일부 코드 추가

--- calc.py
+++ calc.py
@@ -1,1 +1,6 @@
 print("This is an simple calculater.")
+print("Enter any numbers. Don't enter Alphabets.")
+firstnumber = input("First, What is your first number?")
+if isinstance(firstnumber, (int,float)) == 0:
+    print(firstnumber + " is your first number.")
+else: print("This is not a number.")
