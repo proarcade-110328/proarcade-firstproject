dfc10eacbdcc0a14c8bb85131062af67fecfe3b2
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-11-15 13:01:10 +0900

    Added new things

--- calc.py
+++ calc.py
@@ -1,6 +1,45 @@
-print("This is an simple calculater.")
+print("This is an simple calculator.")
 print("Enter any numbers. Don't enter Alphabets.")
-firstnumber = input("First, What is your first number?")
-if isinstance(firstnumber, (int,float)) == 0:
-    print(firstnumber + " is your first number.")
-else: print("This is not a number.")
+def fnumberaction():
+    while True:
+        firstnumber = input("What is your first number?")
+        try:
+            firstnumber = float(firstnumber)
+            return firstnumber
+        except ValueError:
+            print(f"'{firstnumber}' is NOT a number!\n")
+def snumberaction():
+    while True:
+        secondnumber = input("What is your second number?")
+        try:
+            secondnumber = float(secondnumber)
+            return secondnumber
+        except ValueError:
+            print(f"'{secondnumber}' is NOT a number!\n")
+def arithaction(firstnumber, secondnumber):
+    while True:
+        math = input("What kind of calculation do you want to do?")
+        if math == "+":
+            answer = firstnumber + secondnumber
+        elif math == "-":
+            answer = firstnumber - secondnumber
+        elif math == "*":
+            answer = firstnumber * secondnumber
+        elif math == "/" and secondnumber != 0:
+            answer = firstnumber / secondnumber
+        elif math == "/" and secondnumber == 0:
+            print("You can't do this since your second number is 0.\n")
+            continue
+        else:
+            print("You can't do math with that.\n")
+            continue
+        print(answer)
+        return answer
+while True:
+    firstnumber = fnumberaction()
+    secondnumber = snumberaction()
+    answer = arithaction(firstnumber, secondnumber)
+    again = input("Do you want to do another? (y/n)").lower()
+    if again != "y":
+        print("GoodBye!")
+        break
